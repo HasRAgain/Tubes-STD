425165a67782b02060b4611bd6a256a42746609b
Irfaniahimsa <irfaniahimsa28@gmail.com>
2025-12-04 16:46:58 +0700

    update header
    menambahkan primitif insert, delete, dan find

--- pemutarMusik/pemutarMusik.h
+++ pemutarMusik/pemutarMusik.h
@@ -75,10 +75,33 @@ adrAkun createakun(string username, string password, string role);
 void createListAkun(listAkun &La);
 adrLagu createLagu(string idLagu, string judul, string artis, string genre, int durasi);
 void createListLagu(listLagu &Ll);
+adrPlaylist createPlaylist(string nama, string id);
+void createListPlaylist(listPlaylist &LP);
+bool isEmptyPlaylist(listPlaylist LP);
+bool isEmptyAkun(listAkun LA);
+bool isEmptyLagu(listLagu LL);
 adrPlaylistToLagu createRelasiPlaylistToLagu(adrPlaylist p, adrLagu l);
 void createListPlaylistToLagu(listPlaylistToLagu lpl);
 adrPlaylistToAkun createRelasiPlaylistToAkun(adrPlaylist p, adrAkun a);
 void createListPlaylistToAkun(listPlaylistToAkun lpa);
 
+void insertAkun(listAkun &LA, adrAkun A);
+void insertPlaylist(listPlaylist &LP, adrPlaylist P);
+void insertLagu(listLagu &LL, adrLagu L);
+
+void deleteFirstAkun(listAkun &LA, adrAkun &A);
+void deleteAfterAkun((listAkun &LA,adrAkun prec, adrAkun &A);)
+void deleteLastAkun(listAkun &LA, adrAkun &A);
+void deleteFirstLagu(listLagu &LL, adrLagu &L);
+void deleteAfterLagu(listLagu &LL,adrLagu prec, adrLagu &L);
+void deleteLastLagu(listLagu &LL, adrLagu &L);
+void deleteFirstPlaylist(listPlaylist &LP, adrPlaylist &P);
+void deleteAfterPlaylist(listPlaylist &LP,adrPlaylist prec, adrPlaylist &P);
+void deleteLastPlaylist(listPlaylist &LP, adrPlaylist &P);
+
+adrAkun findAkun(listAkun A, string username);
+adrlagu findLaguByID(listLagu L, string id);
+adrLagu findLaguByJudul(listLagu L, string judul);
+adrPlaylist findPlaylist(listPlaylist P, string namaPlaylist);
 
 #endif // PEMUTARMUSIK_H_INCLUDED
